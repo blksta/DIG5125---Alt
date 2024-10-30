9d4859c7ad345f436985173d0769b69b24ef5764
Saint <vicibraimoh@gmail.com>
2024-10-30 15:25:57 +0000

    Plotting Update

--- Tester/Blank.py
+++ Tester/Blank.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import numpy as np 
 import cv2
 
@@ -26,6 +28,8 @@ def processed_image(filename):
 
 
 imagepath = processed_image('Tester\mandrill2.jpg')
+ImageA = imagepath
+ImageB = 'Tester\lena.tiff'
 
 # comparing image sizes
 ImageA_RGB = cv2.imread('Tester\lena.tiff', cv2.IMREAD_COLOR)
@@ -49,3 +53,46 @@ else:
 
 _, ImageA2 = cv2.threshold(ImageA1, 127, 255, cv2.THRESH_BINARY)
 _, ImageB2 = cv2.threshold(ImageA1, 127, 255, cv2.THRESH_BINARY)
+
+ImageC = cv2.bitwise_and(ImageA2, ImageB2)
+
+fig = plt.figure()
+gs = gridspec.GridSpec(2, 5, width_ratios=[1,1,1,2,2], height_ratios=[2,1])
+
+ax1 = plt.subplot(gs[0])
+ax1.imshow(cv2.cvtColor(ImageA_RGB, cv2.COLOR_BGR2RGB))
+ax1.set_title('Original A')
+ax1.axis('off')
+
+ax2 = plt.subplot(gs[1])
+ax2.imshow(ImageA1, cmap='gray')
+ax2.set_title('Gray scale')
+ax2.axis('off')
+
+ax3 = plt.subplot(gs[2])
+ax3.imshow(ImageA2, cmap='gray')
+ax3.set_title('Binary')
+ax3.axis('off')
+
+ax4 = plt.subplot(gs[5])
+ax4.imshow(cv2.cvtColor(ImageB_RGB, cv2.COLOR_BGR2RGB))
+ax4.set_title('Original B')
+ax4.axis('off')
+
+ax5 = plt.subplot(gs[6])
+ax5.imshow(ImageB1, cmap='gray')
+ax5.set_title('Gray scale')
+ax5.axis('off')
+
+ax6 = plt.subplot(gs[7])
+ax6.imshow(ImageB1, cmap='gray')
+ax6.set_title('BW')
+ax6.axis('off')
+
+ax5 = plt.subplot(gs[3:5])
+ax5.imshow(ImageC, cmap='gray')
+ax5.set_title('and Image')
+ax5.axis('off')
+
+plt.tight_layout()
+plt.show()
